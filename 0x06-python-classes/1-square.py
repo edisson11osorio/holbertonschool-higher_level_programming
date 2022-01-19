17add4fd114647a187762f19f7800fe0abf718e4
edisson11osorio <osorioedisson08@gmail.com>
2022-01-18 21:50:14 -0500

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class that defines a square"""
+
+
+class Square:
+    """Write a class Square that defines a square"""
+    def __init__(self, size):
+        self.__size = size
