7e3fc5db3f169dec3e99c208774bbe0ec425f8cf
edisson11osorio <osorioedisson08@gmail.com>
2022-01-31 23:08:18 -0500

    happy_forrr

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a class Square that inherits from Rectangle
+(9-rectangle.py):
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define Square class
+    Instantiation with:
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Implement area method
+        Returns:square size
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        Return a description of Square
+        Returns:[square] <width>/<height>
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
