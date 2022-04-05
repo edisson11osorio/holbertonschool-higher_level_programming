a92bdfc7fc6fbcecb016589d90eb82a9fcb02716
edisson11osorio <osorioedisson08@gmail.com>
2022-04-05 17:24:27 -0500

    fix_semistandard_code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorialize (num) {
-   let MyRes = 1;
-    for (let i = 1; i <= num; i++) {
-      MyRes *= i;
-    }
-    return (MyRes);
+  let MyRes = 1;
+  for (let i = 1; i <= num; i++) {
+    MyRes *= i;
+  }
+  return (MyRes);
 }
 console.log(factorialize(parseInt(process.argv[2])));
