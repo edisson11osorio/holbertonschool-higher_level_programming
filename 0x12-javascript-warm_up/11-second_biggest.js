1eff74a799dfbf3351c6b836a73827e0f4f8f46e
edisson11osorio <osorioedisson08@gmail.com>
2022-04-05 17:29:22 -0500

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function nextBiggest (arr) {
+  let max = 0; let result = 0;
+  for (const value of arr) {
+    const nr = Number(value);
+    if (nr > max) {
+      [result, max] = [max, nr];
+    } else if (nr < max && nr > result) {
+      result = nr;
+    }
+  }
+
+  return result;
+}
+
+console.log(nextBiggest(process.argv));
