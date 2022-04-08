7492c6b959d03f94d27dea00dbeec1b6c28a4c49
edisson11osorio <osorioedisson08@gmail.com>
2022-04-07 20:32:27 -0500

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = (this.width * 2);
+    this.height = (this.height * 2);
+  }
+}
+
+module.exports = Rectangle;
